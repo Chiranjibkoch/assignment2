140a0145cd0c8a640d397ef778e1e20063b438f0
Chiranjib koch <93111779+Chiranjibkoch@users.noreply.github.com>
2023-03-30 21:49:57 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,7 @@
+age = int(input("ᴘʟᴇᴀsᴇ ᴇɴᴛᴇʀ ʏᴏᴜʀ ᴀɢᴇ : "))
+if age < 18:
+    print("ʏᴏᴜ ᴀʀᴇ ᴀ ᴍɪɴᴏʀ")
+elif age >= 18 and age < 65:
+    print("You are ᴀᴅᴜʟᴛ")
+else: 
+    print("You are a senior")
